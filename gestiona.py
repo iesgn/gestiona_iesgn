e4c665dc53542ad93ba20e814e9e028ac06c02a4
José Domingo Muñoz <josedom24@gmail.com>
2016-04-13 10:11:31 +0200

    libldap

--- gestiona.py
+++ gestiona.py
@@ -3,7 +3,10 @@ from bottle import template
 from sesion import get
 
 def tipos(tipo):
+
 	vtipos={"0":"Todos","1":"1ºASIR","2":"2ºASIR","3":"1ºSMR","4":"2ºSMR","5":"Profesor","6":"Antiguo Alumno","7":"Antiguo Profesor","8":"Otro"}
+	if tipo not in vtipos.keys():
+		tipo=8
 	return vtipos[tipo]
 
 def my_template(name,info={}):
