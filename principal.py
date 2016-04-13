97f5a460a9b31b06ee5b575328018d8c5f21cd00
josedom24 <josedom24@gmail.com>
2016-04-13 20:48:26 +0200

    prueba

--- principal.py
+++ principal.py
@@ -1,4 +1,5 @@
 from bottle import route, template, run, static_file, error,request,response,redirect,error
+from wtforms import Form, BooleanField, StringField, validators
 import sesion
 from gestiona import *
 from libldap import LibLDAP
@@ -38,6 +39,18 @@ def usuarios():
         return my_template('usuarios.tpl',info=info)
     else:
         redirect('/')
+
+@route('/prueba')
+def prueba():
+    class RegistrationForm(Form):
+        username     = StringField('Username', [validators.Length(min=4, max=25)])
+        email        = StringField('Email Address', [validators.Length(min=6, max=35)])
+        accept_rules = BooleanField('I accept the site rules', [validators.InputRequired()])
+
+    form=RegistrationForm()
+    info={"form":form}
+    return my_template('prueba.tpl',info=info)
+
 
 @route('/static/<filepath:path>')
 def server_static(filepath):
