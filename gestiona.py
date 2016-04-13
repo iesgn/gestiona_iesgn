49812868f1d49eb1f70624fc1f9579f08c25f3be
José Domingo Muñoz <josedom24@gmail.com>
2016-04-13 10:12:15 +0200

    libldap

--- gestiona.py
+++ gestiona.py
@@ -6,7 +6,7 @@ def tipos(tipo):
 
 	vtipos={"0":"Todos","1":"1ºASIR","2":"2ºASIR","3":"1ºSMR","4":"2ºSMR","5":"Profesor","6":"Antiguo Alumno","7":"Antiguo Profesor","8":"Otro"}
 	if tipo not in vtipos.keys():
-		tipo=8
+		tipo="8"
 	return vtipos[tipo]
 
 def my_template(name,info={}):
