8aa338a6e0b8a17234899fcded67ae18c5cfa84f
josedom24 <josedom24@gmail.com>
2016-04-24 23:06:02 +0200

    Borrar

--- principal.py
+++ principal.py
@@ -97,7 +97,7 @@ def add():
 def borrar(uid):
     if sesion.islogin():
         if request.POST:
-            return request.forms.GET("respuesta")
+            return str(request.forms.GET("respuesta"))
         else:
             info={"uid":uid}
             return my_template('borrar.tpl',info)
