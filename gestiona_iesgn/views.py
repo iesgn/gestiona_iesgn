3626da276c4e1d79dd819595630a514854f9be59
josedom24 <josedom24@gmail.com>
2017-10-08 23:18:36 +0200

    Alumnos

--- gestiona_iesgn/views.py
+++ gestiona_iesgn/views.py
@@ -15,7 +15,7 @@ def index(request):
                 # Solo dejamos loguearse a los alumnos y profesores
                 # No dejamos a los AA y a los AP
                 tipos=[1,2,3,4,5]
-                if info["description"][0] not in tipos:
+                if not info["description"][0] in tipos:
                     info={"error":True}
                     return render(request,"index.html",info)
                 request.session["username"]=username
