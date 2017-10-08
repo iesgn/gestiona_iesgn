966508ad48869f41595d72d21b410b2e8ee3396f
josedom24 <josedom24@gmail.com>
2017-10-08 21:31:01 +0200

    Alumnos

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^/alumnos$', views.listarAlumnos),
+	url(r'^alumnos$', views.listarAlumnos),
 	url(r'^update/(?P<usuario>[0-9]+)$', views.update),
 	url(r'^del/(?P<usuario>[0-9]+)$', views.delete),
 	url(r'^add$', views.add),
