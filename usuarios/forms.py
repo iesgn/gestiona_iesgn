66c6854ac197545679c5ba6520513b47701f3afb
josedom24 <josedom24@gmail.com>
2017-10-08 22:40:09 +0200

    Alumnos

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -22,7 +22,7 @@ class BuscarUsuario(forms.Form):
     nombre=forms.CharField(max_length=100,required=False,widget=forms.TextInput(attrs={'class': "form-control"}))
     apellidos=forms.CharField(max_length=100,required=False,widget=forms.TextInput(attrs={'class': "form-control"}))
     clase=forms.ChoiceField(choices=clasesAlumnos,required=False,widget=forms.Select(attrs={'class': "form-control",'onchange': 'this.form.submit();'}))
-    AP=forms.HiddenField()
+    AP=forms.CharField(widget=forms.HiddenInput())
     def __init__(self, *args, **kwargs):
         super(BuscarUsuario, self).__init__(*args, **kwargs)
         if args[0].has_key("AP") and args[0]["AP"]=="profesores":
