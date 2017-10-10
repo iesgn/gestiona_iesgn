7d0a1615af2dd5f5acc83e309e761ac3675ffb59
José Domingo <josedom24@gmail.com>
2017-10-10 10:12:13 +0200

    cursos

--- cursos/views.py
+++ cursos/views.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from usuarios.views import getLista
 import operator
 # Create your views here.
 
 def cursos(request,curso):
+	clases=["1º ASIR","2º ASIR","1º SMR","2º SMR"]
 	lista=getLista("*","*",[int(curso)])
 	lista.sort(key=operator.itemgetter('sn'))
-	print lista
+	info={"titulo":clases[int(curso)],"resultados":lista}
+	return render(request,"listar.html",info)
