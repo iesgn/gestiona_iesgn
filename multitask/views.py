48f097f8c0368be2bbad86d8ac1364ec7e207a2f
josedom24 <josedom24@gmail.com>
2017-10-15 22:23:04 +0200

    multitask

--- multitask/views.py
+++ multitask/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from gestiona_iesgn.views import test_profesor
+import requests
+import json
+url_base="https://dit.gonzalonazareno.org/redmine/"
 
 # Create your views here.
+
+
+def inicio(request):
+	test_profesor(request)
+	r=requests.get(url_base+'projects.json',auth=(request.session["username"],request.session["password"]),verify=False)
+	if r.status_code == 200:
+		doc=r.json()
+		info={"proyectos":doc["projects"]}
+		return render("proyectos.html",user=username,info)
