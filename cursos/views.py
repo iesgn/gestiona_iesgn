89114d15793f0f7dba5ac65d4bc4f70219430ec2
josedom24 <josedom24@gmail.com>
2017-10-15 11:58:07 +0200

    Cursos

--- cursos/views.py
+++ cursos/views.py
@@ -5,15 +5,9 @@ import operator
 # Create your views here.
 
 def cursos(request,curso):
+	ldap=gnLDAP()
+	filtro["grupo"]=curso
+	lista=ldap.gnBuscar(filtro=filtro)
 	clases=["","1º ASIR","2º ASIR","1º SMR","2º SMR"]
-	tipos=[1,2,3,4,6]
-	tipos.remove(int(curso))
-	lista=getLista("*","*",[int(curso)])
-	lista.sort(key=operator.itemgetter('sn'))
-	#lista2=getLista("*","*",tipos)
-	lista2.sort(key=operator.itemgetter('sn'))
-	todos=[]
-	#for alum in lista2:
-	#	todos.append(alum["uid"][0],alum["cn"][0])
-	info={"titulo":clases[int(curso)],"resultados":lista,"todos":todos}
+	info={"titulo":clases[int(curso)],"resultados":lista}
 	return render(request,"listar_cursos.html",info)
