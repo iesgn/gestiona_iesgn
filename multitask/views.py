96ec571b61bba23054fceaff080189802f69cefd
josedom24 <josedom24@gmail.com>
2017-10-15 23:20:27 +0200

    multitask

--- multitask/views.py
+++ multitask/views.py
@@ -53,39 +53,39 @@ def inicio(request):
         	fecha2=request.POST["fecha2"]
         	resultado=""
        
-        if fecha2!="":
-        	lfecha=fecha2.split("/")
-	        if len(lfecha[1])==1:
-	        	lfecha[1]='0'+lfecha[1]
-	        if len(lfecha[0])==1:
-	        	lfecha[0]='0'+lfecha[0]
-	        fecha2=lfecha[2]+"-"+lfecha[1]+"-"+lfecha[0]
+	        if fecha2!="":
+	        	lfecha=fecha2.split("/")
+		        if len(lfecha[1])==1:
+		        	lfecha[1]='0'+lfecha[1]
+		        if len(lfecha[0])==1:
+		        	lfecha[0]='0'+lfecha[0]
+		        fecha2=lfecha[2]+"-"+lfecha[1]+"-"+lfecha[0]	
+	
 
+	        if opcion=="grupo":
+	        	r=requests.get(url_base+'groups/'+grupo+'.json?include=users',auth=(request.session["username"],request.session["password"]),verify=False)
+	        	if r.status_code == 200:
+	        		doc=r.json()
+	        		alumnos=[]
+	        		for user in doc["group"]["users"]:
+	        			alumnos.append(str(user["id"]))
+	        	
 
-        if opcion=="grupo":
-        	r=requests.get(url_base+'groups/'+grupo+'.json?include=users',auth=(request.session["username"],request.session["password"]),verify=False)
-        	if r.status_code == 200:
-        		doc=r.json()
-        		alumnos=[]
-        		for user in doc["group"]["users"]:
-        			alumnos.append(str(user["id"]))
-        
-
-        resultado=""
-        for alum in alumnos:
-        	r=requests.get(url_base+'/users/'+alum+'.json',auth=(request.session["username"],request.session["password"]),verify=False)
-        	if r.status_code==200:
-        		doc=r.json()
-        		nombre=doc["user"]["firstname"]+" "+doc["user"]["lastname"]
-        	payload = {'issue': {'project_id': idproyecto,'subject': tittle,'description': desc,'category_id': int(categoria),'assigned_to_id': int(alum),'due_date':fecha2}}
-		
-        	parameters_json = json.dumps(payload)
-        	headers = {'Content-Type': 'application/json'}
-        	r = requests.post(url_base+'issues.json', auth=(request.session["username"],request.session["password"]), data=parameters_json, headers=headers,verify=False)
-        	resultado=resultado+nombre+":"+r.reason+"<br/>"
-        info={"idproyecto":idproyecto,"nombreproyecto":nombreproyecto,"resultado":resultado}
-        return render(request,"final.html",info)
-	else:
+	        resultado=""
+	        for alum in alumnos:
+	        	r=requests.get(url_base+'/users/'+alum+'.json',auth=(request.session["username"],request.session["password"]),verify=False)
+	        	if r.status_code==200:
+	        		doc=r.json()
+	        		nombre=doc["user"]["firstname"]+" "+doc["user"]["lastname"]
+	        	payload = {'issue': {'project_id': idproyecto,'subject': tittle,'description': desc,'category_id': int(categoria),'assigned_to_id': int(alum),'due_date':fecha2}}
+			
+	        	parameters_json = json.dumps(payload)
+	        	headers = {'Content-Type': 'application/json'}
+	        	r = requests.post(url_base+'issues.json', auth=(request.session["username"],request.session["password"]), data=parameters_json, headers=headers,verify=False)
+	        	resultado=resultado+nombre+":"+r.reason+"<br/>"
+	        info={"idproyecto":idproyecto,"nombreproyecto":nombreproyecto,"resultado":resultado}
+	        return render(request,"final.html",info)
+    else:
 		r=requests.get(url_base+'projects.json',auth=(request.session["username"],request.session["password"]),verify=False)
 		if r.status_code == 200:
 			doc=r.json()
