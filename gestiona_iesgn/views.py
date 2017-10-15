bc968ebc1e40677e1e8773fdfc35acad73e5ca4d
josedom24 <josedom24@gmail.com>
2017-10-15 21:57:10 +0200

    Comprobar login

--- gestiona_iesgn/views.py
+++ gestiona_iesgn/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from usuarios.libldap import LibLDAP
+from usuarios.libldap import gnLDAP
 from django.http import Http404
 def index(request):
     if request.method=="GET":
@@ -7,20 +7,20 @@ def index(request):
     else:
         username = request.POST["username"]
         password = request.POST["password"].encode('utf-8')
-        lldap=LibLDAP(username,password)
+        lldap=gnLDAP(username,password)
         if lldap.isbind:
                 busqueda='(uid=%s)'%username
                 resultados=lldap.buscar(busqueda)
                 info=resultados[0].get_attributes()
                 # Solo dejamos loguearse a los alumnos y profesores
                 # No dejamos a los AA y a los AP
-                tipos=["1","2","3","4","5"]
-                if not info["description"][0] in tipos:
+                tipos=["asir1","asir2","smr1","smr2","profesores"]
+                if not lldap.memberOfGroup(request.POST["username"],key=True) in tipos:
                     info={"error":True}
                     return render(request,"index.html",info)
                 request.session["username"]=username
                 request.session["password"]=password
-                if info["gidNumber"][0]=="2000":
+                if lldap.memberOfGroup(request.POST["username"],key=True)=="profesores":
                     request.session["profesor"]=True
                 else:
                     request.session["profesor"]=False
