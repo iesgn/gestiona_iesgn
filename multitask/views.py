58a07a2156437a4d5d35beb88995594add5a8d9a
josedom24 <josedom24@gmail.com>
2017-10-15 22:37:13 +0200

    multitask

--- multitask/views.py
+++ multitask/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from gestiona_iesgn.views import test_profesor
+from django.views.decorators.csrf import csrf_protect
 import requests
 import json
 url_base="https://dit.gonzalonazareno.org/redmine/"
 
 # Create your views here.
 
-
+@csrf_protect
 def inicio(request):
 	test_profesor(request)
 	r=requests.get(url_base+'projects.json',auth=(request.session["username"],request.session["password"]),verify=False)
