947b4db29e5272babe59b667985665a0a69f832d
josedom24 <josedom24@gmail.com>
2017-10-16 16:39:39 +0200

    Documentacion wifi

--- documentacion/views.py
+++ documentacion/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import Http404
+from gestiona_iesgn.views import test_profesor,test_login
 
 # Create your views here.
 
 def doc(request,doc):
+	islogin=["ca"]
+	isprofesor=[]
+	if doc in islogin:
+		test_login(request)
+	if doc in isprofesor:
+		test_profesor(request)
 	try:
 		return render(request,doc+".html")
 	except:
