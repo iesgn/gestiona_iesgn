9a38227cd36adad531f803d2ab560a0a199d9dee
josedom24 <josedom24@gmail.com>
2017-10-18 23:56:12 +0200

    correos

--- correos/forms.py
+++ correos/forms.py
@@ -27,7 +27,7 @@ class BuscarDestinatariosForm(forms.Form):
             lista=[("0","Ninguno"),("asir1","1º ASIR"),("asir2","2º ASIR"),("smr1","1º SMR"),("smr2","2º SMR"),("antiguosalumnos","A.A.")]
             ldap=gnLDAP()
             
-            self.fields["alumnos"] = forms.ChoiceField(initial= alum,choices=sorted(lista),required=False,widget=forms.Select(attrs={'class': "form-control",'onchange': 'this.form.submit();'}))
+            self.fields["alumnos"] = forms.ChoiceField(initial= alum,choices=lista,required=False,widget=forms.Select(attrs={'class': "form-control",'onchange': 'this.form.submit();'}))
             self.fields["destinatarios"]=forms.MultipleChoiceField(initial=dest,choices=getSelect(),required=False,widget=forms.SelectMultiple(attrs={'class': "form-control js-example-basic-multiple"}))
 
             
