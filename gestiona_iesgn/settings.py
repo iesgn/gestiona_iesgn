41986161558cc48bb01816a5ae6ba13c8bb5ca95
josedom24 <josedom24@gmail.com>
2017-10-18 15:27:52 +0200

    site url

--- gestiona_iesgn/settings.py
+++ gestiona_iesgn/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_URL = SITE_URL_STATIC
+STATIC_URL = '/gestiona/static'
 STATICFILES_DIRS = (
             # Put strings here, like "/home/html/static" or "C:/www/django/static".
             # Always use forward slashes, even on Windows.
