267f4d68bdbb1136d416f340d8f677351e5f251f
josedom24 <josedom24@gmail.com>
2017-10-18 23:19:14 +0200

    correos

--- correos/views.py
+++ correos/views.py
@@ -8,8 +8,10 @@ from usuarios.libldap import gnLDAP
 def add(request):
 	if request.method=='POST' and not request.POST.has_key("correo"):
 		form2 = BuscarDestinatariosForm(request.POST)
-		print request.POST.get("asunto")
-		form = CorreoForm(asunto=request.POST.get("asunto"),dest=SelectUsuarios(request.POST.get("Alumnos")))
+		info={}
+		info["asunto"]=request.POST.get("asunto")
+		info["contenido"]=request.POST.get("contenido")		
+		form = CorreoForm(info,dest=SelectUsuarios(request.POST.get("Alumnos")))
 #    elif request.method=='POST' and request.POST.has_key("correo"):
 #        form2 = BuscarDestinatariosForm(request.POST.get("Profesores")) 
 #        form = CorreoForm(request.POST)
