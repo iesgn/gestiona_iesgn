52b46aecb926ca97e4f3709abcda2601ec391763
josedom24 <josedom24@gmail.com>
2017-10-19 17:40:10 +0200

    Correos

--- correos/forms.py
+++ correos/forms.py
@@ -47,6 +47,6 @@ class BuscarDestinatariosForm(forms.Form):
 			ldap=gnLDAP()
 			
 			self.fields["alumnos"] = forms.ChoiceField(initial=alum,choices=lista,required=False,widget=forms.Select(attrs={'class': "form-control",'onchange': 'this.form.submit();'}))
-			self.fields["destinatarios"]=forms.MultipleChoiceField(required=True,initial=dest,choices=getSelect(),required=False,widget=forms.SelectMultiple(attrs={'class': "form-control js-example-basic-multiple"}))
+			self.fields["destinatarios"]=forms.MultipleChoiceField(initial=dest,choices=getSelect(),required=True,widget=forms.SelectMultiple(attrs={'class': "form-control js-example-basic-multiple"}))
 
 			
