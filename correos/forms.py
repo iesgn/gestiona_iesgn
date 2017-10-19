7a29310f638c3069f29932fb36c97e0802e7c2b6
josedom24 <josedom24@gmail.com>
2017-10-19 17:23:35 +0200

    Correos

--- correos/forms.py
+++ correos/forms.py
@@ -5,29 +5,40 @@ from django.forms.widgets import HiddenInput,Textarea,TextInput
 from usuarios.libldap import gnLDAP
 
 def getSelect():
-    ldap=gnLDAP()
-    
-    lista=ldap.gnBuscar(cadena="(uid=*)")
-    lista2=[]
-    for usuario in lista:
-        lista2.append((usuario["uid"][0],usuario["givenname"][0]+" "+usuario["sn"][0]))
-    return lista2
+	ldap=gnLDAP()
+	
+	lista=ldap.gnBuscar(cadena="(uid=*)")
+	lista2=[]
+	for usuario in lista:
+		lista2.append((usuario["uid"][0],usuario["givenname"][0]+" "+usuario["sn"][0]))
+	return lista2
 
 class CorreoForm(forms.Form):
-    asunto=forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class': "form-control"}))
-    contenido=forms.CharField(max_length=100,widget=forms.Textarea(attrs={'class': "form-control",'cols': 100, 'rows': 15}))
+	asunto=forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class': "form-control"}))
+	contenido=forms.CharField(max_length=100,widget=forms.Textarea(attrs={'class': "form-control",'cols': 100, 'rows': 15}))
+	def clean(self):
+		 super(CorreoForm, self).clean()
+		 if self.instance.pk:
+			asunto = self.cleaned_data.get("asunto")
+			if not asunto:
+				 del self._errors['asunto']
+			contenido = self.cleaned_data.get("contenido")
+			if not contenido:
+				 del self._errors['contenido']
+
+		 return self.cleaned_data
 
 class BuscarDestinatariosForm(forms.Form):
-    
-    def __init__(self, *args, **kwargs):
-            dest = kwargs.pop('dest')
-            alum = kwargs.pop('alum')
-            
-            super(BuscarDestinatariosForm, self).__init__(*args, **kwargs)
-            lista=[("0","Ninguno"),("asir1","1º ASIR"),("asir2","2º ASIR"),("smr1","1º SMR"),("smr2","2º SMR"),("antiguosalumnos","A.A."),("profesores","Profesores")]
-            ldap=gnLDAP()
-            
-            self.fields["alumnos"] = forms.ChoiceField(initial=alum,choices=lista,required=False,widget=forms.Select(attrs={'class': "form-control",'onchange': 'this.form.submit();'}))
-            self.fields["destinatarios"]=forms.MultipleChoiceField(initial=dest,choices=getSelect(),required=False,widget=forms.SelectMultiple(attrs={'class': "form-control js-example-basic-multiple"}))
+	
+	def __init__(self, *args, **kwargs):
+			dest = kwargs.pop('dest')
+			alum = kwargs.pop('alum')
+			
+			super(BuscarDestinatariosForm, self).__init__(*args, **kwargs)
+			lista=[("0","Ninguno"),("asir1","1º ASIR"),("asir2","2º ASIR"),("smr1","1º SMR"),("smr2","2º SMR"),("antiguosalumnos","A.A."),("profesores","Profesores")]
+			ldap=gnLDAP()
+			
+			self.fields["alumnos"] = forms.ChoiceField(initial=alum,choices=lista,required=False,widget=forms.Select(attrs={'class': "form-control",'onchange': 'this.form.submit();'}))
+			self.fields["destinatarios"]=forms.MultipleChoiceField(initial=dest,choices=getSelect(),required=False,widget=forms.SelectMultiple(attrs={'class': "form-control js-example-basic-multiple"}))
 
-            
+			
