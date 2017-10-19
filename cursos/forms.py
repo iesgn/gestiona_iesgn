4ff7c63c215e81784c5c2714fb85f14215cca0c6
josedom24 <josedom24@gmail.com>
2017-10-19 20:13:26 +0200

    Varios grupos

--- cursos/forms.py
+++ cursos/forms.py
@@ -17,18 +17,12 @@ class BuscarUsuario(forms.Form):
 	grupo=forms.CharField(widget=forms.HiddenInput())
 	def __init__(self, *args, **kwargs):
 		super(BuscarUsuario, self).__init__(*args, **kwargs)
-		if args[0].has_key("grupo") and args[0]["grupo"]=="openstackusers":
-			grupos=["asir1","asir2","smr1","smr2","profesores"]
-			lista=[]
-			for grupo in grupos:
-				lista.extend(getSelect(grupo))
-			lista2=getSelect("openstackusers")
-			lista3 = [x for x in lista if x not in lista2]
-			self.fields['usuarios']=forms.MultipleChoiceField(choices=lista3,required=False,widget=forms.SelectMultiple(attrs={'class': "form-control js-example-basic-multiple"}))
+		lista=getSelect('alumnos')+getSelect('profesores')
+		lista2=getSelect(args[0]["grupo"])
+		lista3 = [x for x in lista if x not in lista2]
+		self.fields['usuarios']=forms.MultipleChoiceField(choices=lista3,required=False,widget=forms.SelectMultiple(attrs={'class': "form-control js-example-basic-multiple"}))
 
-		elif args[0].has_key("grupo"):
-			self.fields['usuarios']=forms.MultipleChoiceField(choices=getSelect("antiguosalumnos"),required=False,widget=forms.SelectMultiple(attrs={'class': "form-control js-example-basic-multiple"}))
-
+		
         
 
 
