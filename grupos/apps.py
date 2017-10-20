4cf9fa0afc604abd20b91ebf4cf1f7e008e6a5d2
josedom24 <josedom24@gmail.com>
2017-10-20 20:57:11 +0200

    Nuevo modelo de grupos

--- grupos/apps.py
+++ grupos/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class CursosConfig(AppConfig):
-    name = 'cursos'
+class GruposConfig(AppConfig):
+    name = 'grupos'
