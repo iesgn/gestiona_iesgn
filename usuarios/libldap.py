ff3a4167553c70736c70eb8067363646b3f95565
josedom24 <josedom24@gmail.com>
2017-10-20 17:27:43 +0200

    Nuevo modelo de grupos

--- usuarios/libldap.py
+++ usuarios/libldap.py
@@ -112,14 +112,14 @@ class gnLDAP(LibLDAP):
                         grupos=["asir1","asir2","smr1","smr2"]
                     elif valor=="allprofesores":
                         grupos=["profesores","antiguosprofesores"]
-                    elif valor=="all":
-                        grupos=["asir1","asir2","smr1","smr2","antiguosalumnos","profesores","antiguosprofesores"]
                     cadena2="(|"
                     for grupos in grupos:
                         cadena2+="(memberOf=cn=%s,ou=Group,dc=gonzalonazareno,dc=org)" % grupos
                     cadena2+=")"
                     cadena+=cadena2
-                    print cadena
+                
+                elif campo=="grupo" and valor=='all': 
+                    cadena+="(uid=*)"
                 else:
                     cadena+="(%s=%s*)" % (campo,valor)
             cadena+=")"
