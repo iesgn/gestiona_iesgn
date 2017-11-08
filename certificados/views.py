3ed3944dedd22abdd44062bed085c5062a1b6281
josedom24 <josedom24@gmail.com>
2017-11-08 19:28:04 +0100

    certificados

--- certificados/views.py
+++ certificados/views.py
@@ -37,7 +37,8 @@ def handle_uploaded_file(f,nombre):
 def download(request,usuario,file):
 	test_login(request)
 	if usuario==request.session["username"]:
-		filename = str(os.path.join(settings.BASE_DIR, 'cert/%s'%(request.session["username"]+"/"+file.encode("utf8"))))
+		filename = str(os.path.join(settings.BASE_DIR, 'cert/%s'%(request.session["username"]+"/"+file)))
+		print "*"*20
 		print filename
 		wrapper = FileWrapper(file(filename))
 		response = HttpResponse(wrapper, content_type='text/plain')
