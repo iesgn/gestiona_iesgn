ad396159ba7a218f6182d182e5e1976685df70cf
josedom24 <josedom24@gmail.com>
2017-11-08 15:55:05 +0100

    certificados

--- certificados/views.py
+++ certificados/views.py
@@ -9,18 +9,17 @@ def add(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request)
+            handle_uploaded_file(request.FILES["csr"],request.session("username"))
             return redirect(settings.SITE_URL+'/certificados')
     else:
         form = UploadFileForm()
     return render(request, 'upload.html', {'form': form})
 
-def handle_uploaded_file(request):
-	path= os.path.join(settings.BASE_DIR, 'cert/%s'%request.session("username"))
+def handle_uploaded_file(f,nombre):
+	path= os.path.join(settings.BASE_DIR, 'cert/%s'%nombre)
 	if not os.path.isdir(path):
 		os.mkdir(path)
-	path_file=path+"/"+request.FILES['csr'].name
-	f=request.FILES['csr']
+	path_file=path+"/"+f.name
 	with open(path_file, 'w') as destination:
 		destination.write(f.read())
     
