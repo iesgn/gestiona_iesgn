9b8865b0a7b99cfcd14ef251582e4fa860b561e1
josedom24 <josedom24@gmail.com>
2017-11-08 19:26:15 +0100

    certificados

--- certificados/views.py
+++ certificados/views.py
@@ -39,7 +39,7 @@ def download(request,usuario,file):
 	if usuario==request.session["username"]:
 		filename = os.path.join(settings.BASE_DIR, 'cert/%s'%(request.session["username"]+"/"+file.encode("utf8")))
 		print filename
-		wrapper = FileWrapper(file(filename)
+		wrapper = FileWrapper(file(filename))
 		response = HttpResponse(wrapper, content_type='text/plain')
 		response['Content-Disposition'] = 'attachment; filename=%s' % os.path.basename(filename)
 		response['Content-Length'] = os.path.getsize(filename)
