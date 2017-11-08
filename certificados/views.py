fecdc275d88b8748f3ff40143486e936eb5c389b
josedom24 <josedom24@gmail.com>
2017-11-08 19:36:32 +0100

    certificados

--- certificados/views.py
+++ certificados/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse
 from .forms import UploadFileForm
 from django.conf import settings
 from gestiona_iesgn.views import test_login
