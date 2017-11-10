d13f4f51986f2f559e9ff0c1ba693335c597c266
josedom24 <josedom24@gmail.com>
2017-11-10 16:03:39 +0100

    certificados

--- certificados/forms.py
+++ certificados/forms.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 from django import forms
 
 class UploadFileFormEquipo(forms.Form):
-    csr_equipo = forms.FileField(label="Peición de certificado de equipo. Sube el fichero csr:")
+    csr_equipo = forms.FileField(label="Petición de certificado de equipo. Sube el fichero csr:")
 class UploadFileFormUsuario(forms.Form):
-    csr_usuario = forms.FileField(label="Peición de certificado de usuario. Sube el fichero csr:")
+    csr_usuario = forms.FileField(label="Petición de certificado de usuario. Sube el fichero csr:")
