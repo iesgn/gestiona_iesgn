4bfc5b239fab332f7e9b2be764d0a1fe1603bf11
josedom24 <josedom24@gmail.com>
2017-11-10 20:02:35 +0100

    certificados

--- certificados/urls.py
+++ certificados/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.add),
 	url(r'^(?P<usuario>[0-9a-z.]+)/usuario/(?P<file>[0-9a-z.]+)$', views.download_usuario),
-	url(r'^(?P<usuario>[0-9a-z.]+)/equipo/(?P<direc>[0-9]+)/(?P<file>[0-9a-z.]+)$', views.download_equipo),
+	url(r'^(?P<usuario>[0-9a-z.]+)/equipo/(?P<direc>[0-9a-z.]+)/(?P<file>[0-9a-z.]+)$', views.download_equipo),
 	
   ]
