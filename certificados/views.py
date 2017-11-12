6fb595cd4e0321f8014319ac247f3d65efe2ec5f
josedom24 <josedom24@gmail.com>
2017-11-12 12:19:46 +0100

    certificados

--- certificados/views.py
+++ certificados/views.py
@@ -127,7 +127,7 @@ def revocar(request,usuario,direc="",file=""):
 		os.rename(filename,filename+"_revocar")
 		asunto="Petición de revocación de certificado de "+tipo+" de " + str(request.session["username"])
 		file=str(file)
-		cuerpo="%s %s: %s"%(request.session["username"],tipo,file)
+		cuerpo="El usuario "+usuario+" ha solicitado la revocación de su certificado de "+tipo+":"+file
 		email = EmailMessage(
  				   asunto,
 				   cuerpo,
