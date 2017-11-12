6a40832bedde52917df5cce21631b365329383e4
josedom24 <josedom24@gmail.com>
2017-11-12 11:45:07 +0100

    certificados

--- certificados/urls.py
+++ certificados/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 	url(r'^(?P<usuario>[0-9a-z.]+)/usuario/(?P<file>[0-9a-z.]+)$', views.download),
 	url(r'^(?P<usuario>[0-9a-z.]+)/equipo/(?P<direc>[0-9a-z.]+)/(?P<file>[0-9a-z.]+)$', views.download),
 	url(r'^(?P<usuario>[0-9a-z.]+)/equipo/(?P<direc>[0-9a-z.]+)/(?P<file>[0-9a-z.]+)/del$', views.revocar),
+	url(r'^(?P<usuario>[0-9a-z.]+)/equipo/(?P<direc>[0-9a-z.]+)/(?P<file>[0-9a-z.]+)/del$', views.revocar),
 	
   ]
