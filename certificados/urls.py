f7b5f11f443d24bc1229fc71e35d61dd82832c17
josedom24 <josedom24@gmail.com>
2018-10-10 15:56:50 +0200

    Change url for cert

--- certificados/urls.py
+++ certificados/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.add),
-	url(r'^(?P<usuario>[\w.@+-]+)/usuario/(?P<file>[\w.]{0,256})$', views.download),
-	url(r'^(?P<usuario>[\w.@+-]+)/equipo/(?P<direc>[0-9a-z.]+)/(?P<file>[\w.]{0,256})$', views.download),
-	url(r'^(?P<usuario>[\w.@+-]+)/equipo/(?P<direc>[0-9a-z.]+)/(?P<file>[\w.]{0,256})/del$', views.revocar),
-	url(r'^(?P<usuario>[\w.@+-]+)/usuario/(?P<file>[\w.]{0,256})/del$', views.revocar),
+	url(r'^(?P<usuario>[\w.@+-]+)/usuario/(?P<file>[\w.-]{0,256})$', views.download),
+	url(r'^(?P<usuario>[\w.@+-]+)/equipo/(?P<direc>[0-9a-z.]+)/(?P<file>[\w.-]{0,256})$', views.download),
+	url(r'^(?P<usuario>[\w.@+-]+)/equipo/(?P<direc>[0-9a-z.]+)/(?P<file>[\w.-]{0,256})/del$', views.revocar),
+	url(r'^(?P<usuario>[\w.@+-]+)/usuario/(?P<file>[\w.-]{0,256})/del$', views.revocar),
 	
   ]
