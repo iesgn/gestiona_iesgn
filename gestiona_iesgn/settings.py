e68a448c24c477a4ce20993b0e5b97289572ce7c
José Domingo Muñoz Rodríguez <josedom24@gmail.com>
2019-09-06 10:33:48 +0200

    Configuración de correos

--- gestiona_iesgn/settings.py
+++ gestiona_iesgn/settings.py
@@ -136,3 +136,5 @@ STATICFILES_DIRS = (
             # Don't forget to use absolute paths, not relative paths.
             os.path.join(BASE_DIR,"static"),
                 )
+
+EMAIL_HOST = 'correo.gonzalonazareno.org'
