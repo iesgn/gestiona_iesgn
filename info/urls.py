925118d334003c48ea7cff83b669038573fb2e69
José Domingo Muñoz <josedom24@gmail.com>
2020-06-13 21:42:28 +0200

    documentación

--- info/urls.py
+++ info/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+  url(r'^actualizar',views.actualizar),
 	url(r'^(?P<tipo>[a-z0-9]+)$', views.doc),
 	url(r'^(?P<tipo>[a-z0-9]+)/(?P<url>[a-z0-9]+)$', views.show),
   ]
--- info/views.py
+++ info/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import Http404
 from gestiona_iesgn.views import test_profesor,test_login
 from django.conf import settings
 import os
 import markdown
 
+def actualizar(request):
+	path = os.path.join(settings.STATICFILES_DIRS[0], 'content_iesgn/')
+	os.system("cd %s && git pull" % path)
+	return redirect("/")
+
 def doc(request,tipo):
 	#datos=getInfo(tipo)
-	datos=getInfoVisibility(tipo,"public")
+	visibility="public"
+	print(request.session)
+	
+	if request.session.get("username"):
+		visibility="auth"
+	
+	if request.session.get("profesor"):
+		visibility="profesor"
+	datos=getInfoVisibility(tipo,visibility)
 	if len(datos)==0:
 		raise Http404 
 	info={"datos":datos,"tipo":tipo}
@@ -26,13 +39,13 @@ def getInfo(tipo):
 	datos=[]
 	for root, dirs, files in os.walk(path):
 		for file in files:
-			with open(path+file, "r", encoding="utf-8") as input_file:
-				text = input_file.read()
-			md = markdown.Markdown(extensions = ['meta'])
-			html = md.convert(text)
-			meta=md.Meta
-			datos.append({"html":html,"meta":meta})
-	print(datos)
+			if root==path:
+				with open(path+file, "r", encoding="utf-8") as input_file:
+					text = input_file.read()
+				md = markdown.Markdown(extensions = ['meta'])
+				html = md.convert(text)
+				meta=md.Meta
+				datos.append({"html":html,"meta":meta})
 	datos.sort(key=lambda item:int(item['meta']['order'][0]), reverse=True)
 	return datos
 
@@ -42,18 +55,24 @@ def getDoc(tipo,url,session):
 		if dato.get("meta").get("url")[0]==url:
 			if dato.get("meta").get("visibility")[0]=="public":
 				return dato
-			elif dato.get("meta").get("visibility")[0]=="auth" and session.username:
+			elif dato.get("meta").get("visibility")[0]=="auth" and session.get("username"):
 				return dato
-			elif dato.get("meta").get("visibility")[0]=="profesor" and session.profesor:
+			elif dato.get("meta").get("visibility")[0]=="profesor" and session.get("profesor"):
 				return dato
 	return None
 
 def getInfoVisibility(tipo,visibility):
 	datos=getInfo(tipo)
 	respuesta=[]
+	print(visibility)
 	for dato in datos:
-		if dato.get("meta").get("visibility")[0]==visibility:
+		if visibility=="profesor":
 			respuesta.append(dato)
+		elif visibility=="auth" and (dato.get("meta").get("visibility")[0]=="auth" or dato.get("meta").get("visibility")[0]=="public"):
+			respuesta.append(dato)
+		elif visibility=="public" and dato.get("meta").get("visibility")[0]=="public":
+			respuesta.append(dato)
+			
 	return respuesta
 
 
