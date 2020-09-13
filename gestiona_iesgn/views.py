27f7b93a2986bfd52f5b1c0d8d49dbd1b3ae57d6
José Domingo Muñoz Rodríguez <josedom24@gmail.com>
2020-09-13 16:44:10 +0200

    Fecha últimas entradas del blog

--- gestiona_iesgn/views.py
+++ gestiona_iesgn/views.py
@@ -17,7 +17,7 @@ def index(request):
     ####
     ## Generamos últimas entradas del blog
     datos=getInfoVisibility("blog",visibility)
-    info["blog"]=datos[:8]
+    info["blog"]=datos[:5]
 
     if request.method=="GET":
         return render(request,'index.html',info)
