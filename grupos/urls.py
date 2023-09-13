6975bb23d8de56179da8e3bb2705e95ab6ee7ccf
José Domingo Muñoz <josedom24@gmail.com>
2023-09-13 12:38:33 +0200

    Proxmox

--- grupos/urls.py
+++ grupos/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^(?P<curso>[a-z0-9]+)$', views.cursos),
-	url(r'^delete/(?P<curso>[a-z0-9]+)/(?P<usuario>[a-z0-9.]+)$', views.eliminar),
+	url(r'^delete/(?P<curso>[a-z0-9]+)/(?P<usuario>[a-zA-Z0-9.]+)$', views.eliminar),
   ]
