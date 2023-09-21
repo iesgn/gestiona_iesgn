16fc2921f180ffa7eda53f882f6b304a3c3fb5e1
José Domingo Muñoz <josedom24@gmail.com>
2023-09-21 12:24:34 +0200

    url con mayúsculas

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.listarUsuarios),
-	url(r'^update/(?P<usuario>[\- 0-9a-z.]+)$', views.update),
+	url(r'^update/(?P<usuario>[\- 0-9A-Za-z.]+)$', views.update),
 	url(r'^add$', views.add),
 	url(r'^perfil$', views.perfil),
 	url(r'^del$', views.delete),
