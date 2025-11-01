9e6f4259ed4aa5c09684284863a27abcf37f8ae3
José Domingo Muñoz <josedom24@gmail.com>
2025-11-01 22:38:01 +0100

    Empresas

--- empresas/urls.py
+++ empresas/urls.py
@@ -4,15 +4,15 @@ from . import views
 app_name = "empresas"
 
 urlpatterns = [
-    path("", views.lista_empresas, name="lista"),
-    path("nueva/", views.nueva_empresa, name="crear"),
-    path("<int:pk>/editar/", views.editar_empresa, name="editar"),
-    path("<int:pk>/borrar/", views.borrar_empresa, name="borrar"),
-    path("<int:pk>/historial/", views.historial_empresa, name="historial"),
+    re_path(r'^$', views.lista_empresas, name="lista"),
+    re_path(r'^nueva/', views.nueva_empresa, name="crear"),
+    re_path(r'^<int:pk>/editar/', views.editar_empresa, name="editar"),
+    re_path(r'^<int:pk>/borrar/', views.borrar_empresa, name="borrar"),
+    re_path(r'^<int:pk>/historial/', views.historial_empresa, name="historial"),
     # Nuevas acciones:
-    path('<int:pk>/alumnos/', views.gestionar_alumnos, name='alumnos'),
-    path('<int:pk>/contactos/', views.gestionar_contactos, name='contactos'),
-    path('contacto/<int:contacto_id>/borrar/', views.borrar_contacto, name='borrar_contacto'),
-    path('alumno/<int:alumno_id>/historial/', views.historial_alumno, name='historial_alumno'),
+    re_path(r'^<int:pk>/alumnos/', views.gestionar_alumnos, name='alumnos'),
+    re_path(r'^<int:pk>/contactos/', views.gestionar_contactos, name='contactos'),
+    re_path(r'^contacto/<int:contacto_id>/borrar/', views.borrar_contacto, name='borrar_contacto'),
+    re_path(r'^alumno/<int:alumno_id>/historial/', views.historial_alumno, name='historial_alumno'),
 
 ]
